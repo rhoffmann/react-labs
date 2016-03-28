cd7b79baf92e11efaedaa52781a09d10ac8b9578
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2016-03-28 13:48:43 +0200

    alert is alert

--- src/modules/global/Alert.js
+++ src/modules/global/Alert.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const Message = ({ type, message }) => {
+const Alert = ({ type, message }) => {
   const classes = classnames('alert', `alert-${type}`);
   return (
     <div className={classes}>{message}</div>
   );
 };
 
-Message.propTypes = {
+Alert.propTypes = {
   type: React.PropTypes.string,
   message: React.PropTypes.string
 };
 
-export default Message;
+export default Alert;
