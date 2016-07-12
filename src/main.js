66b93ced3da5880a9113c9eec9f89d57838d15c5
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2016-07-12 15:43:52 +0200

    removed hashHistory

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 require('./main.scss');
